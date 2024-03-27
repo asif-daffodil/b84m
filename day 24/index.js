10f37926ab61020828d63d016da8bf5854beb088
asif-daffodil <abir@dipti.com.bd>
2024-03-27 13:45:36 +0600

    3272024145pm

---
+++ day 24/index.js
@@ -0,0 +1,76 @@
+// variable
+
+var msg = 'Hello World';
+console.log(msg);
+
+// this is a single line comment
+
+/* 
+    this is a
+    multiline
+    comment
+*/
+
+// data types
+/**
+ * string
+ * number
+ * boolean
+ * object
+ */
+
+console.log(typeof "Hello World");
+console.log(typeof 123);
+console.log(typeof 123.456);
+console.log(typeof true);
+console.log(typeof [1,2,3]);
+console.log(typeof {personName : 'Asif'});
+console.log(typeof null);
+
+/**
+ * Arithmetic operators
+ * +
+ * -
+ * *
+ * /
+ * %
+ */
+
+/**
+ * Assignment operators
+ * =
+ * +=
+ * -=
+ * *=
+ * /=
+ * %=
+ * **
+ */
+
+/**
+ * concatenation operator
+ * +
+ * +=
+ */
+
+/**
+ * comparison operator
+ * ==
+ * ===
+ * !=
+ * !==
+ * <
+ * <=
+ * >
+ * >=
+ */
+
+
+/**
+ * conditional operator
+ * &&
+ * ||
+*/
+
+
+
